77b9732c60f6d1c0ad5f5fbbc37e25e87b0cfbdc
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-07 21:34:38 +0100

    update the movie routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('movies', [MoviesController::class, 'index']);
-Route::get('movies/{id}', [MoviesController::class, 'show']);
-Route::post('movies', [MoviesController::class, 'store']);
-Route::put('movies/{id}', [MoviesController::class, 'update']);
-Route::delete('movies/{id}', [MoviesController::class, 'delete']);
 
