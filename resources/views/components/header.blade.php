4b225958e0735a1a990fa0cfef7d4b55538ca12f
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-08 00:03:36 +0100

    modify header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,16 +20,16 @@
         <a href="#tab1Id" class="nav-link active" data-bs-toggle="tab">Active</a>
     </li>
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">{{Session::get('user')['name']}}</a>
         <div class="dropdown-menu">
-            <a class="dropdown-item" href="#tab2Id">Action</a>
-            <a class="dropdown-item" href="#tab3Id">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#tab4Id">Action</a>
+            <a class="dropdown-item" href="/logout">Logout</a>
+           
         </div>
     </li>
     <li class="nav-item" role="presentation">
-        <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Another link</a>
+        <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Cinema 1</a>
+        <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Cinema 2</a>
+        <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Cinema 3</a>
     </li>
    
 </ul>
