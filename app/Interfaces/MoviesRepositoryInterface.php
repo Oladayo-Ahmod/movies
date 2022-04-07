d38517078933f0492798fa21a5316a0cb3a466cc
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-07 21:32:01 +0100

    update the movie interface

--- app/Interfaces/MoviesRepositoryInterface.php
+++ app/Interfaces/MoviesRepositoryInterface.php
@@ -5,9 +5,9 @@ namespace App\Interfaces;
 interface MoviesRepositoryInterface 
 {
     public function getAllMovies();
-    public function getMovieById($movieId);
-    public function deleteMovie($movieId);
-    public function createMovie(array $movieDetails);
-    public function updateMovie($movieId, array $newDetails);
+    public function getMoviesById($movieId);
+    public function deleteMovies($movieId);
+    public function createMovies(array $movieDetails);
+    public function updateMovies($movieId, array $newDetails);
     // public function getFulfilledMovies();
 }
